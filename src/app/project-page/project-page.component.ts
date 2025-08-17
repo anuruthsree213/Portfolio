70fe410500ab6d1950e677f3b7adc9fbab76ebbf
Anuruth Sree <anuruthsree213@gmail.com>
2025-08-17 14:36:37 +0530

    Profile and header change

--- src/app/project-page/project-page.component.ts
+++ src/app/project-page/project-page.component.ts
@@ -7,38 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectPageComponent implements OnInit {
 
-  projects = [
-    {
-      title: "Telecom Churn Analysis",
-      description: "Predicted customer churn using ML models, improving retention strategies.",
-      image: "assets/projects/churn.png",
-      link: "https://github.com/anuruthsree213/churn-analysis"
-    },
-    {
-      title: "Banking Fraud Detection",
-      description: "Built a fraud detection pipeline using Python + SQL + Power BI dashboards.",
-      image: "assets/projects/fraud.png",
-      link: "https://github.com/anuruthsree213/fraud-detection"
-    },
-    {
-      title: "AI Resume Evaluator",
-      description: "Developed an AI-based resume screening app using open-source LLMs.",
-      image: "assets/projects/resume.png",
-      link: "https://github.com/anuruthsree213/resume-evaluator"
-    },
-    {
-      title: "Banking Fraud Detection",
-      description: "Built a fraud detection pipeline using Python + SQL + Power BI dashboards.",
-      image: "assets/projects/fraud.png",
-      link: "https://github.com/anuruthsree213/fraud-detection"
-    },
-    {
-      title: "AI Resume Evaluator",
-      description: "Developed an AI-based resume screening app using open-source LLMs.",
-      image: "assets/projects/resume.png",
-      link: "https://github.com/anuruthsree213/resume-evaluator"
-    }
-  ];
+  projects = [];
+  // projects = [
+  //   {
+  //     title: "Telecom Churn Analysis",
+  //     description: "Predicted customer churn using ML models, improving retention strategies.",
+  //     image: "assets/projects/churn.png",
+  //     link: "https://github.com/anuruthsree213/churn-analysis"
+  //   }
+  // ];
 
   constructor() { }
 
