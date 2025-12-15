6e57c44e5bb0586f3a4334614363f084b01390ff
Anuruth Sree <anuruthsree213@gmail.com>
2025-12-15 21:45:00 +0530

    project desc update

--- src/app/project-page/project-page.component.ts
+++ src/app/project-page/project-page.component.ts
@@ -11,7 +11,7 @@ export class ProjectPageComponent implements OnInit {
   projects = [
     {
       title: "AI PDF Summarizer",
-      description: "An AI-powered PDF summarization tool that extracts key insights from large PDF documents. The project uses an Angular frontend for a simple and responsive UI, and a Python FastAPI backend integrated with an open-source LLM (like Mistral or LLaMA 2) to generate summaries.",
+      description: "An AI-powered PDF summarization tool that extracts key insights from large PDF documents. The project uses an Angular frontend for a simple and responsive UI, and a Python FastAPI backend integrated with Gemini to generate summaries.",
       link: "https://anuruthsree213.github.io/pdfSummarizer"
     }
   ];
